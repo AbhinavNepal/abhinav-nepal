c7ccf0fbd2529a70135d468f6feb6cddc0ff934b
Biwek Shrestha <biweks@gmail.com>
2018-03-14 22:40:46 +0000

    introduced pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "scholars#index"
 
   resources :scholars, only: [:index]
+
+  # pages
+  get "about" => "pages#about"
+  get "privacy" => "pages#privacy"
 
 end
